bae8cb1aa22b4fc1c7e0605f2a659d9cd79ec49c
quangduccao <onggieoi@gmail.com>
2021-10-03 10:15:44 +0700

    remove secret keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyDGgBoVUM9cR7XdE4izU4rTjU_xgAltnsQ",
-    authDomain: "angular-f81d0.firebaseapp.com",
-    projectId: "angular-f81d0",
-    storageBucket: "angular-f81d0.appspot.com",
-    messagingSenderId: "25257869425",
-    appId: "1:25257869425:web:3406a0b17810f7bc2a9bd0"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   },
 };
